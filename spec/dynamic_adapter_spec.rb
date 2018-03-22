67be4ac210ba792815b5f38a05d77994c87f4ba3
vmusulainen <vmusulainen@gmail.com>
2018-03-22 12:27:30 +0300

    dynamic_adapter_spec.rb is fixed

--- spec/dynamic_adapter_spec.rb
+++ spec/dynamic_adapter_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__),'spec_helper')
+require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Rhoconnect::Model::DynamicAdapterModel" do
   include_examples "SharedRhoconnectHelper", :rhoconnect_data => true
@@ -6,33 +6,33 @@ describe "Rhoconnect::Model::DynamicAdapterModel" do
   it "should return login when backend service defined" do
     stub_request(:post, "http://test.com/rhoconnect/authenticate").to_return(:body => "lucas")
     Rhoconnect.appserver = 'http://test.com'
-    Rhoconnect::Model::DynamicAdapterModel.authenticate('lucas','').should == 'lucas'
+    Rhoconnect::Model::DynamicAdapterModel.authenticate('lucas', '').should == 'lucas'
   end
 
   it "should query dynamic adapter service" do
-    data = {'1'=>@product1}
+    data = {'1' => @product1}
     stub_request(:post, "http://test.com/rhoconnect/query").with(:headers => {'Content-Type' => 'application/json'}).to_return(:status => 200, :body => data.to_json)
-    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2,nil,'http://test.com')
+    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2, nil, 'http://test.com')
     da.query.should == data
   end
 
   it "should create new object using dynamic adapter" do
-    stub_request(:post, "http://test.com/rhoconnect/create").with(:headers => {'Content-Type' => 'application/json'}).to_return(:body => {:id => 5})
-    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2,nil,'http://test.com')
-    da.create(@product1).should == {:id => 5}
+    stub_request(:post, "http://test.com/rhoconnect/create").with(:headers => {'Content-Type' => 'application/json'}).to_return(:body => {:id => 5}.to_json)
+    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2, nil, 'http://test.com')
+    da.create(@product1).should == {:id => 5}.to_json
   end
 
   it "should update object using dynamic adapter" do
     data = {'id' => 2}
-    stub_request(:post, "http://test.com/rhoconnect/update").with(:headers => {'Content-Type' => 'application/json'}).to_return(:body => {:id => 5})
-    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2,nil,'http://test.com')
-    da.update(data).should == {:id => 5}
+    stub_request(:post, "http://test.com/rhoconnect/update").with(:headers => {'Content-Type' => 'application/json'}).to_return(:body => {:id => 5}.to_json)
+    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2, nil, 'http://test.com')
+    da.update(data).should == {:id => 5}.to_json
   end
 
   it "should delete object using dynamic adapter" do
     data = {'id' => 2}
-    stub_request(:post, "http://test.com/rhoconnect/delete").with(:headers => {'Content-Type' => 'application/json'}).to_return(:body => {:id => 5})
-    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2,nil,'http://test.com')
-    da.delete(data).should == {:id => 5}
+    stub_request(:post, "http://test.com/rhoconnect/delete").with(:headers => {'Content-Type' => 'application/json'}).to_return(:body => {:id => 5}.to_json)
+    da = Rhoconnect::Model::DynamicAdapterModel.new(@s2, nil, 'http://test.com')
+    da.delete(data).should == {:id => 5}.to_json
   end
 end
